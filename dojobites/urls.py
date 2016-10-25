28f7e22d40bd8d24ca5d821c681ab535064a8e09
antcho92 <antcho92@gmail.com>
2016-10-25 08:44:36 -0700

    added dojobites_app models to admin panel

--- dojobites/urls.py
+++ dojobites/urls.py
@@ -1,26 +1,21 @@
-"""dojobites URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 
 from apps.login_reg_app.models import User
+from apps.dojobites_app.models import Restaurant, Date, Comment
 
 class UserAdmin(admin.ModelAdmin):
     pass
 admin.site.register(User, UserAdmin)
+class RestaurantAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Restaurant, RestaurantAdmin)
+class DateAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Date, DateAdmin)
+class CommentManager(admin.ModelAdmin):
+    pass
+admin.site.register(Comment, CommentManager)
 
 
 urlpatterns = [
