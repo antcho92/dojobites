f3787988be835288a7e0255fbeab344e1e49a659
antcho92 <antcho92@gmail.com>
2016-10-27 09:19:35 -0700

    fixed new restaurant form to reflect our new model

--- apps/dojobites_app/models.py
+++ apps/dojobites_app/models.py
@@ -21,9 +21,7 @@ class RestaurantManager(models.Manager):
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    cuisine = models.CharField(max_length=255)
-    takeout = models.BooleanField()
+    rating = models.IntegerField()
     location = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
