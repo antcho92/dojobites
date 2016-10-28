89b71737c3e5dc2898861efd671d71416cc25959
Yubao Zhao <zhaoyubao1981@gmail.com>
2016-10-28 08:33:45 -0700

    Update styles.css

--- apps/dojobites_app/models.py
+++ apps/dojobites_app/models.py
@@ -7,14 +7,11 @@ class RestaurantManager(models.Manager):
     def validate_restaurant(self, input):
         errors = []
         name = input['name']
-        rating = input['rating']
         location = input['location']
         if not name or name.isspace():
             errors.append('Please enter the name!')
         elif self.filter(name__iexact=name).exists():
             errors.append('Restaurant already existed!')
-        if not rating or rating.isspace():
-            errors.append('Please enter the rating!')
         if errors:
             return (False, errors)
         self.create(name=name, rating=rating, location=location)
