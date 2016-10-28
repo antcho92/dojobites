f23448aae5a649225fa34d5dfc1dae3ae336bba1
Evanyfc <evanyfc@gmail.com>
2016-10-28 12:32:50 -0700

    added marker and new parameters for google maps api

--- apps/dojobites_app/static/dojobites_app/js/map.js
+++ apps/dojobites_app/static/dojobites_app/js/map.js
@@ -2,19 +2,34 @@ var map;
 var infowindow;
 
 window.initMap = function() {
-  var codingdojo = {lat: 47.609811, lng: -122.196526};
+  var codingdojo = {lat: 47.609999, lng: -122.196985};
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: codingdojo,
     zoom: 16
   });
 
+  var goldStar = {
+    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+    fillColor: 'yellow',
+    fillOpacity: 0.8,
+    scale: 0.1,
+    strokeColor: 'gold',
+    strokeWeight: 3
+  };
+
+  var marker = new google.maps.Marker({
+    position: codingdojo,
+    icon: goldStar,
+    map: map
+  });
+
   infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch({
     location: codingdojo,
-    radius: 450,
-    type: ['food'],
+    radius: 500,
+    type: ['restaurant'],
     rankBy: google.maps.places.RankBy.PROMINENCE
   }, callback);
 }
